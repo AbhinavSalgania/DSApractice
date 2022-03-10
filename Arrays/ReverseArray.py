377caaad628dbbdf6c39ed7b8b37a0aa4a5a1c0d
Abhinav <abhinavsalgania@gmail.com>
2022-03-10 02:12:31 -0330

    Reverse Array finished

--- Arrays/ReverseArray.py
+++ Arrays/ReverseArray.py
@@ -1,5 +1,10 @@
-print("Enter array begin: ", end="")
-a = int(input())
+testcases = int(input())  # number of test cases
 
-print("Enter array end: ", end="")
-z = int(input())
+for i in range(0, testcases):
+    n = int(input())  # Enter number of elements
+
+    arr = input().split()
+    for item in arr[::-1]:
+        print(item, end=" ")
+
+    print("\n")
