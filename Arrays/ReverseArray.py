599f047900ba1f95ffa6a5ca49b96f27d1a105bd
Abhinav <abhinavsalgania@gmail.com>
2022-03-15 16:58:45 -0230

    Reverse Array polished

--- Arrays/ReverseArray.py
+++ Arrays/ReverseArray.py
@@ -1,10 +1,12 @@
 testcases = int(input())  # number of test cases
+print("Enter numbers with space in them")
+if testcases <= 100:
+    for i in range(0, testcases):
 
-for i in range(0, testcases):
-    n = int(input())  # Enter number of elements
+        arr = input().split()
+        for item in arr[::-1]:
+            print(item, end=" ")
 
-    arr = input().split()
-    for item in arr[::-1]:
-        print(item, end=" ")
-
-    print("\n")
+        print("\n")
+else:
+    print("Error: Enter a number less than or equal to 100")
